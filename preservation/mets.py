e6a345b7514d73f1aa116b6f7c381dc4026c0088
Mikko Vatanen <mikko.vatanen@csc.fi>
2015-10-06 18:37:37 +0300

    KDKPAS-1034 Library for reading/writing generic METS XML documents

---
+++ preservation/mets.py
@@ -0,0 +1,48 @@
+"""Read and write METS documents"""
+
+METS_NS = 'http://www.loc.gov/METS/'
+XSI_NS = 'http://www.w3.org/2001/XMLSchema-instance'
+
+
+def mets_ns(tag, prefix=""):
+    """Prefix ElementTree tags with METS namespace.
+
+    object -> {info:lc...premis}object
+
+    :tag: Tag name as string
+    :returns: Prefixed tag
+
+    """
+    if prefix:
+        tag = tag[0].upper() + tag[1:]
+        return '{%s}%s%s' % (METS_NS, prefix, tag)
+    return '{%s}%s' % (METS_NS, tag)
+
+
+def get_created_date(mets):
+    """Return createdate and modified date from given mets document.
+
+    CREATEDATE
+    LASTMODDATE
+
+    :mets: ElementTree document
+    :returns: (createdate, modifieddate)
+
+    """
+
+    header = mets.find(mets_ns('metsHdr'))
+    create_date = header.get("CREATEDATE")
+    last_modified_date = header.get("LASTMODDATE")
+
+    return (create_date, last_modified_date)
+
+
+def get_objid(mets):
+    """Return mets:OBJID from given `mets` document
+
+    :mets: ElementTree document
+    :returns: objid
+
+    """
+
+    return mets.get("OBJID")
---
+++ tests/mets_test.py
@@ -0,0 +1,45 @@
+"""Test the `preservation.mets` module"""
+
+import pytest
+
+
+from preservation.workflow.utils import read_xml
+
+from luigi import LocalTarget
+
+import preservation.mets as m
+
+
+@pytest.fixture
+def mets():
+    """Return a test mets.xml file
+
+    :returns: METS as ElementTree
+
+    """
+    return read_xml(LocalTarget(
+        'tests/workflow/data/csc_test_valid_sip/mets.xml')).getroot()
+
+
+def test_find_created_date():
+    """Test the `preservation.mets.find_created_date` function
+
+    :returns: None
+
+    """
+
+    (created_date, last_modified_date) = m.get_created_date(mets())
+
+    assert created_date == '2013-11-19T16:40:17'
+    assert last_modified_date is None
+
+
+def test_get_objid():
+    """Test the `preservation.mets.get_objid` function.
+
+    :returns: None
+
+    """
+
+    objid = m.get_objid(mets())
+    assert objid == 'kdk-csc-sip001'
