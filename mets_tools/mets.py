f16a160fde3fd8f6fc7c37c5a0392b0b180bdf86
Juha Lehtonen <juha.lehtonen@csc.fi>
2017-09-13 17:05:07 +0300

    KDKPAS-1433 Removed commented code.

--- mets_tools/mets.py
+++ mets_tools/mets.py
@@ -23,9 +23,6 @@ def mets_mets(profile, objid=str(uuid.uuid4()), label=None,
         xsi_ns('schemaLocation'),
         'http://www.loc.gov/METS/ '
         'http://www.loc.gov/standards/mets/mets.xsd')
-    # mets.set('xmlns:' + 'mets', METS_NS)
-    # mets.set('xmlns:' + 'xsi', XSI_NS)
-    # mets.set('xmlns:' + 'xlink', XLINK)
     mets.set('PROFILE', profile)
     mets.set('OBJID', objid)
     if label:
