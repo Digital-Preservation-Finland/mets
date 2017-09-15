2037b6b27cf9c047bef5bf8312eb99e26f381912
Juha Lehtonen <juha.lehtonen@csc.fi>
2017-09-15 17:01:09 +0300

    KDKPAS-1433 Added child element possibility to mdwrap and xmldata.

--- mets/mdwrap.py
+++ mets/mdwrap.py
@@ -4,15 +4,21 @@ from mets.mets import _element
 
 
 def mdwrap(mdtype='PREMIS:OBJECT', othermdtype="",
-           mdtypeversion="2.3"):
+           mdtypeversion="2.3", child_elements=None):
     elem = _element('mdWrap')
     elem.set('MDTYPE', mdtype)
     elem.set('MDTYPEVERSION', mdtypeversion)
     if mdtype == 'OTHER':
         elem.set('OTHERMDTYPE', othermdtype)
+    if child_elements:
+        for elem in child_elements:
+            dmdsec_elem.append(elem)
     return elem
 
 
-def xmldata():
-    return _element('xmlData')
-
+def xmldata(child_elements=None):
+    xmldata_e = _element('xmlData')
+    if child_elements:
+        for elem in child_elements:
+            dmdsec_elem.append(elem)
+    return xmldata_e
