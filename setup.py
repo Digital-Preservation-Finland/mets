0d9e4b6d4cbfb5619a2339c900e23f6c35f92e37
Heikki Mikkolainen <heikki.mikkolainen@csc.fi>
2019-01-31 16:02:03 +0200

    TPASPKT-211 Use PEP 508 URL dependencies instead of dependency links

--- setup.py
+++ setup.py
@@ -12,9 +12,12 @@ def main():
         name='mets',
         packages=find_packages(exclude=['tests', 'tests.*']),
         version=get_version(),
-        install_requires=["lxml", 'python-dateutil', 'xml_helpers'],
-        dependency_links=[('git+https://gitlab.csc.fi/dpres/xml-helpers.git'
-                           '@develop#egg=xml_helpers-0')]
+        install_requires=[
+            'lxml',
+            'python-dateutil',
+            'xml_helpers@git+https://gitlab.csc.fi/dpres/xml-helpers.git'
+            '@develop'
+        ]
     )
 
 
